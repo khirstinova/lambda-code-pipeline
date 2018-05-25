3c1616e7c1614ec21ca912a2f351d9eb7d2abeda
khirst <khirst@inovasolutions.com>
2018-05-25 09:21:40 -0400

    1.) Added mocha tests.

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
+var AWS = require('aws-sdk');
 var time = require('time');
-exports.handler = (event, context, callback) => {
-    var currentTime = new time.Date(); 
-    currentTime.setTimezone("America/Los_Angeles");
-    callback(null, {
-        statusCode: '200',
-        body: 'The time in Los Angeles is: ' + currentTime.toString(),
+
+exports.handler = function (event, context, callback) {
+    var iot = new AWS.Iot({'region': 'us-east-1', apiVersion: '2015-05-28'});
+
+    iot.describeCertificate({
+        certificateId: '655bb742b4ddf0188bfced154ae9cf932bd6597a31ff19cbd79e12456a6d5e21'
+    }, function (err, data) {
+        console.log("Data: " + data);
     });
 }
---
+++ test.js
@@ -0,0 +1,22 @@
+var assert = require('assert');
+var index = require('./index');
+var AWS = require('aws-sdk-mock');
+ 
+describe('index', function() {
+  describe('#handler()', function() {
+    it('should complete without error', function() {
+
+      AWS.mock('Iot', 'describeCertificate', function (params, callback){
+          callback(null, "successfully put item in database");
+      });
+        
+      index.handler({ /* event */ }, { /* context */ }, function (err, data) {
+        AWS.restore('Iot', 'describeCertificate');
+        done();
+      });
+    });
+  });
+});
+
+
+
